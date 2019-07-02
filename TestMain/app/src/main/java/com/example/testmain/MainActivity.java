5bd1e17004a6573c3a7ae59500912bf075d650b8
nhusonnb <tranninhnb90@gmail.com>
2019-07-02 19:24:09 +0700

    new

---
+++ TestMain/app/src/main/java/com/example/testmain/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.testmain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
